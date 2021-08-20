684a65299ac38815b7d2f404ab65683c22b3d755
wiz1108 <wiz1108@hotmail.com>
2021-08-20 12:11:09 -0700

    repair appbar

---
+++ src/components/AppBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import './AppBar.css'
+import { Link } from 'react-router-dom'
+import { Icon } from '@material-ui/core'
+import { AccountCircle } from '@material-ui/icons'
+const AppBar = ({ user, setUser, username, setUsername }) => {
+	return (
+		<div className='appBar'>
+			<div className='slider'>
+				<Sidebar setUsername={setUsername} />
+				<Link to='/' className='home'>
+					<b>Quiz</b>
+				</Link>
+			</div>
+			<div className='appBar-user'>
+				<Icon>
+					<AccountCircle />
+				</Icon>
+				<p>{user.name}{username}</p>
+			</div>
+		</div>
+	)
+}
+
+export default AppBar
